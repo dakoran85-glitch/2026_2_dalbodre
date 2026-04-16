4d9229ccfc340e7d38e7fd7a6fb2b5ace5e965a3
dakoran85-glitch <dakoran85@gmail.com>
2026-04-16 12:12:36 +0900

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // 디자인을 입혀주는 핵심 줄입니다!
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
